87e4bc47054052dc27b1d8b9804432f5f2b3d70f
mamiddd <100997069+mamiddd@users.noreply.github.com>
2022-07-19 07:42:14 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const ALLOWED_GROUPS = []
-export const BOT_TOKEN = ''
-export const ROBOT_NAME = '@javbus_bot'
+export const BOT_TOKEN = '5461061229:AAHYh4UkZCIwMOky_gJ7iZZ1ImDc-LS0mYw'
+export const ROBOT_NAME = '@hupanbot'
